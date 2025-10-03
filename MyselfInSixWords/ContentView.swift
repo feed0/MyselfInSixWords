18b9a4e544327ad997c649c6d4d57ce5361f07c5
feed0 <duda@duck.com>
2025-10-03 15:10:41 -0300

    1* MyQualities

--- MyselfInSixWords/ContentView.swift
+++ MyselfInSixWords/ContentView.swift
@@ -7,18 +7,55 @@
 
 import SwiftUI
 
+let myQualities = ["Resilient",
+                   "Dedicated",
+                   "Patient",
+                   "Learner",
+                   "Polyglot",
+                   "Healthy",]
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(myQualities, id: \.self) { quality in
+                Adjective(quality)
+            }
         }
         .padding()
     }
 }
 
+struct Adjective: View {
+    
+    // MARK: - Constants
+    let paddingLength = 8.0
+    let backGroundColor = Color.blue
+    let backGroundColorOpacity = 0.5
+    let cornerRadius = 8.0
+    let shadowRadius = 8.0
+    let foreGroundColor = Color.white
+    
+    
+    // MARK: - Parameters
+    let literal: String
+    
+    // MARK: - Init
+    init(_ literal: String) {
+        self.literal = literal
+    }
+    
+    // MARK: - Body
+    var body: some View {
+        Text(literal)
+            .foregroundColor(foreGroundColor)
+            .padding(paddingLength)
+            .background(backGroundColor.opacity(backGroundColorOpacity),
+                        in: RoundedRectangle(cornerRadius: cornerRadius))
+            .shadow(radius: shadowRadius)
+    }
+}
+
+// MARK: - Preview
 #Preview {
     ContentView()
 }
