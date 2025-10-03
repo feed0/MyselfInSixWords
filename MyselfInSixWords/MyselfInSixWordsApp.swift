fd7375341696a36c652a58bbf172e4484ce83969
feed0 <duda@duck.com>
2025-10-03 14:48:20 -0300

    Initial Commit

---
+++ MyselfInSixWords/MyselfInSixWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyselfInSixWordsApp.swift
+//  MyselfInSixWords
+//
+//  Created by Felipe Campelo on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MyselfInSixWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
